d3f674802fd6079cf92d63a281de905114309b97
Firman <fhaya.firman@gmail.com>
2021-09-14 16:03:58 +0700

    add docker-compose

--- src/server.js
+++ src/server.js
@@ -45,7 +45,7 @@ const init = async () => {
 
   await server.start();
   // eslint-disable-next-line no-console
-  console.log(`Server berjalan pada ${server.info.uri}`);
+  console.log(`Server running on ${server.info.uri}`);
 };
 
 init();
